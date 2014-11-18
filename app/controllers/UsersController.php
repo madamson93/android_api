cbf48d948e917da3fea8bf497fa73a8157af2b22
Monique Adamson <madamson@hotmail.co.uk>
2014-11-18 20:41:47 +0000

    Initial Commit
    Created migration for users table, defined route and resource for API

---
+++ app/controllers/UsersController.php
@@ -0,0 +1,100 @@
+<?php
+
+class UsersController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//displays all users
+		
+		return('Display all users.');
+
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+		
+		return('Show form to create new user.');
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//creates a new user
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//get a specific user by their id
+		
+		return('Display user by their id.');
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	
+		return('Form to edit user.');
+	}
+	
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//PATH request to update the user details by their id
+		
+		return('PATCH request to update user details by their id.');
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//delete a user from the table
+		
+		return('Delete a user from the table');
+	}
+
+
+}
---
+++ app/database/migrations/2014_11_18_192731_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('users', function($table)
+        {
+                $table->increments('userid');
+                
+                $table->string('name', 50);
+                $table->string('email');
+                $table->string('encrypted_password', 80);
+                $table->string('salt', 10);
+                
+                $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
