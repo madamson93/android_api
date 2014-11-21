02dff457239502fec441ac0cb1efddeede1aa71c
Monique Adamson <madamson@hotmail.co.uk>
2014-11-21 13:40:35 +0000

    Editing environment setup
    Adding in arrays for domains for local and production environments

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,8 +26,10 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+    //'local' => array('homestead'),
 
+    'local' => array('*.dev', gethostname()),
+    'production' => array('*.com', '*.net', 'www.somedomain.com')
 ));
 
 /*
